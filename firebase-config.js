d6a9525c1aec4297d513760bc7395722dcb5d550
Copsa-ai <sgaleano@petroleracopsa.com.ar>
2026-09-16 15:35:05 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 // Los sacás en: Consola de Firebase > Configuración del proyecto (ícono de engranaje)
 // > "Tus apps" > app web > Config (SDK setup and configuration).
 export const firebaseConfig = {
-  apiKey: "AIzaSyBYo_t_W-hZ7QNQLW9HJqul95WZISyYsF0",
-  authDomain: "detalle-de-entregas-copsa.firebaseapp.com",
-  projectId: "detalle-de-entregas-copsa",
-  storageBucket: "detalle-de-entregas-copsa.firebasestorage.app",
-  messagingSenderId: "319258529169",
-  appId: "1:319258529169:web:0269edb096d5e938f5de30",
+  apiKey: "AIzaSyC3e-urEaj3mjPLbIyffEqJh-fCE_jq3YM",
+  authDomain: "monitor-ruteo-diario.firebaseapp.com",
+  projectId: "monitor-ruteo-diario",
+  storageBucket: "monitor-ruteo-diario.firebasestorage.app",
+  messagingSenderId: "1098383050561",
+  appId: "1:1098383050561:web:9443a8eb35b570e4bb43c0",
 };
 
 // PIN que van a usar los choferes para poder entrar y marcar entregas.
